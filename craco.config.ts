02c406d4bb7f56a3e2c7587999317347e0a382fc
Mike Nedelko <mnedelko@gmail.com>
2023-06-03 06:17:47 -0500

    refactored v2

--- craco.config.ts
+++ craco.config.ts
@@ -1,28 +1,10 @@
-const webpack = require("webpack");
-
 const config = {
-  webpack: {
-    configure: {
-      resolve: {
-        fallback: {
-          process: require.resolve("process/browser"),
-          zlib: require.resolve("browserify-zlib"),
-          stream: require.resolve("stream-browserify"),
-          util: require.resolve("util"),
-          buffer: require.resolve("buffer"),
-          asset: require.resolve("assert"),
-          http: require.resolve("stream-http"),
-          https: require.resolve("https-browserify"),
-          crypto: require.resolve("crypto-browserify")
-        },
-      },
-      plugins: [
-        new webpack.ProvidePlugin({
-          Buffer: ["buffer", "Buffer"],
-          process: "process/browser",
-        }),
-      ],
-    },
+  babel: {
+    plugins: [
+      "@babel/plugin-proposal-nullish-coalescing-operator",
+      "@babel/plugin-proposal-optional-chaining",
+      "@babel/plugin-transform-shorthand-properties",
+    ],
   },
 };
 export default config;
