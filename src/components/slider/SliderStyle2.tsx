6c94c35664e535ecd5275463aeefb69cfd15431e
Mike Nedelko <mnedelko@gmail.com>
2023-06-10 16:31:20 -0500

    we swapped to public key

--- src/components/slider/SliderStyle2.tsx
+++ src/components/slider/SliderStyle2.tsx
@@ -688,7 +688,7 @@ const SliderStyle2 = (props: SliderProps) => {
     
             const mintResult = await mintOneToken(
               candyMachine,
-              publicKey,
+              anchorWallet.publicKey,
               beforeTransactions,
               afterTransactions,
               setupMint ?? setupTxn
