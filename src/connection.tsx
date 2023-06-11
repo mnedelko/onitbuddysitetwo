619c50c66432cb53076dab1907f7db5d09622bdf
Mike Nedelko <mnedelko@gmail.com>
2023-06-10 22:49:27 -0500

    we changed the equal

--- src/connection.tsx
+++ src/connection.tsx
@@ -167,15 +167,15 @@ import {
   //This is where we left
   console.log("unsignedTxns", unsignedTxns);
 
-  const test = unsignedTxns[0].signatures.find((sig)=> sig.publicKey === wallet.publicKey);
+  const test = unsignedTxns[0].signatures.find((sig)=> sig.publicKey == wallet.publicKey);
   console.log("This is a test", test);
 
   const partiallySignedTransactions = unsignedTxns.filter((t) =>
-      t.signatures.find((sig) => sig.publicKey === wallet.publicKey));
+      t.signatures.find((sig) => sig.publicKey == wallet.publicKey));
   console.log("partiallySignedTransactions", partiallySignedTransactions);
   console.log("wallet.publicKey", wallet.publicKey);
   const fullySignedTransactions = unsignedTxns.filter(
-      (t) => !t.signatures.find((sig) => sig.publicKey === wallet.publicKey));
+      (t) => !t.signatures.find((sig) => sig.publicKey == wallet.publicKey));
   console.log("fullySignedTransactions", fullySignedTransactions);
   let signedTxns = await wallet.signAllTransactions(
       partiallySignedTransactions
