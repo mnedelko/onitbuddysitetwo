c1854a77481d2458299a54844566eee2990fcb6d
Mike Nedelko <mnedelko@gmail.com>
2023-06-18 21:09:52 -0500

    changed logging

--- src/connection.tsx
+++ src/connection.tsx
@@ -186,8 +186,6 @@ import { CandyMachineAccount } from "./candy-machine";
     const test = unsignedTxns[0].signatures.find((sig)=> sig.publicKey.equals(candymachine.program.provider.wallet.publicKey));
     console.log("This is a testly", test);
 
-
-
     console.log("wallet.publiKey", candymachine.program.provider.wallet.publicKey);
 
     const partiallySignedTransactions = unsignedTxns.filter((t) =>
